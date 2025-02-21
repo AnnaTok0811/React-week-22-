e435f4a50bbb204f82e0ce6d76f051ec799c57a0
AnnaTok0811 <nekrasova-a-s@mail.ru>
2025-02-21 10:47:23 +0300

    21.02

---
+++ src/App/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.scss'
+import Header from '../components/Header/Header.jsx'
+import CardList from '../components/Card/CardList.jsx';
+
+function App() {
+  
+
+  return (
+    <>
+    <Header/>
+    <CardList/>
+    </>
+  )
+}
+
+export default App
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,25 +1,25 @@
-import PropTypes from 'prop-types';
-import './Card.css';
+
+import './Card.scss'
 
 
 function Card(props) {
     
 				//здесь мы берем конкретные свойства, которые будут задаваться 
 				//при вызове этого компонента
-				const {title, description, alterego, work, imgLink} = props;
+				const {name, universe, alterego, occupation, imgLink} = props;
         return (
             <div className="card">
                 <div className="card-body">
-                    <h2 className="card-title">{title}</h2>
-                    <img className="card-img" src={imgLink} alt={title}/>
-                    <p className="card-text">Вселенная: {description}</p>
+                    <h2 className="card-title">{name}</h2>
+                    <img className="card-img" src={imgLink} alt={name}/>
+                    <p className="card-text">Вселенная: {universe}</p>
                     <p className="card-text">Альтер эго: {alterego}</p>
-                    <p className="card-text">Род деятельности: {work}</p>
+                    <p className="card-text">Род деятельности: {occupation}</p>
                 </div>
             
             </div>
         );
     
 }
-Card.propsTypes={title:PropTypes.string};
-export default Card;
+
+export default Card
